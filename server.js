99d8470112c474e1fbcfa1591f0216e282eaab46
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-09 13:27:05 -1000

    resolved corrections to final answer

--- server.js
+++ server.js
@@ -4,41 +4,30 @@ const net = require("net");
 const files = require("./files.js");
 
 const PORT = 8080;
-let date = new Date();
-
-let HTTPrequest = "";
 
 const server = net.createServer(socket => {
+  let date = new Date();
+  let HTTPrequest = "";
+
   socket.on("data", chunk => {
     // parse the string
-    let dataStuff = chunk.toString();
-    let url = dataStuff.split("\n")[0];
+    let dataBody = chunk.toString();
+    let url = dataBody.split("\n")[0];
 
     // grab the right file
     if (url.indexOf("/helium.html") !== -1) {
-      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${
-        files.helium.length
-      }\nConnection: keep-alive\n\n${files.helium}`;
+      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${files.helium.length}\nConnection: keep-alive\n\n${files.helium}`;
     } else if (url.indexOf("/hydrogen.html") !== -1) {
-      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${
-        files.hydrogen.length
-      }\nConnection: keep-alive\n\n${files.hydrogen}`;
+      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${files.hydrogen.length}\nConnection: keep-alive\n\n${files.hydrogen}`;
     } else if (
       url.indexOf("/index.html") !== -1 ||
       url.indexOf("GET / HTTP") !== -1
     ) {
-      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${
-        files.index.length
-      }\nConnection: keep-alive\n\n${files.index}`;
-      console.log(HTTPrequest);
+      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${files.index.length}\nConnection: keep-alive\n\n${files.index}`;
     } else if (url.indexOf("/styles.css") !== -1) {
-      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${
-        files.styles.length
-      }\nConnection: keep-alive\n\n${files.styles}`;
+      HTTPrequest = `HTTP/1.1 200 OK\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${files.styles.length}\nConnection: keep-alive\n\n${files.styles}`;
     } else {
-      HTTPrequest = `HTTP/1.1 404 Page Not Found\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${
-        files.err404
-      }\nConnection: keep-alive\n\n${files.err404}`;
+      HTTPrequest = `HTTP/1.1 404 Page Not Found\nServer: matrix/9.9.9\nDate: ${date}\nContent-Type: text/html; charset=utf-8\nContent-Length: ${files.err404}\nConnection: keep-alive\n\n${files.err404}`;
     }
 
     // write outgoing data
